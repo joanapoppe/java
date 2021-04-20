270e9eecea82c0f8e2d5398fb8153f06ce2fad33
Joana Poppe <70990288+joanapoppe@users.noreply.github.com>
2021-04-20 20:04:44 -0300

    Add files via upload

---
+++ massacorporal.java
@@ -0,0 +1,13 @@
+package massacorporal;
+public class massacorporal (
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in) ;
+        double peso, alt, imc;
+        System.out.print("Peso...");
+        peso = Double.parseDoube(sc.nextline());
+        System.out.print("Altura");
+        alt = Doube.parseDouble(sc.nextLine());
+        imc = peso / (alt*alt)
+        System.out.printIn("IMC" + imc);
+    }
+)
